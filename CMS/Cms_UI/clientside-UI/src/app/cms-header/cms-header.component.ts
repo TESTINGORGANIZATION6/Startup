9241ee1ba5e274456376a3d1f2e1ac0314bf8992
swapnilrabse <61693277+swapnilrabse@users.noreply.github.com>
2020-03-15 12:34:29 +0530

    added CMS code
    added CMS code

--- CMS/Cms_UI/clientside-UI/src/app/cms-header/cms-header.component.ts
+++ CMS/Cms_UI/clientside-UI/src/app/cms-header/cms-header.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-cms-header',
@@ -7,17 +8,61 @@ import { Router } from '@angular/router';
   styleUrls: ['./cms-header.component.scss']
 })
 export class CmsHeaderComponent implements OnInit {
+  @Input() isclub: boolean;
+  @Output() activetab = new EventEmitter();
+  username = "swapnil";
+  activeTab: any;
 
-  username="swapnil";
-  activeTab:any;
   constructor(private router: Router) { }
 
 
   ngOnInit() {
-    this.activeTab=this.router.url.split('/')[2];
-  //   this.router.events.subscribe((res) => {
-  //    this.ac
-  // })
+    var app = this.router.url.split('/')[1];
+    if (app == 'dashboard') {
+      this.activeTab = (localStorage.getItem('cmsActive-tab')) ? localStorage.getItem('cmsActive-tab') : 'players';
+      this.activetab.emit(this.activeTab);
+    } else {
+      this.activeTab = 'users';
+      this.activeTab = (localStorage.getItem('portfolioActive-tab')) ? localStorage.getItem('portfolioActive-tab') : 'users';
+      this.activetab.emit(this.activeTab);
+    }
+    //   this.router.events.subscribe((res) => {
+    //    this.ac
+    // })
+  }
+  openMenu() {
+    document.getElementById("mySidenav").style.width = "250px";
   }
 
+  closeMenu() {
+    document.getElementById("mySidenav").style.width = "0px";
+  }
+  changeCms(cmsName) {
+    if (cmsName == "Portfolio") {
+      // this.portfolioActive=true;
+      this.router.navigate(['portfolio']);
+      localStorage.removeItem('cmsActive-tab');
+
+    } else {
+      //  this.portfolioActive=false;
+      this.router.navigate(['dashboard']);
+      localStorage.removeItem('portfolioActive-tab');
+
+    }
+    this.closeMenu()
+  }
+
+  loadTabs(tabname, app) {
+    this.activeTab = tabname;
+    if (app == 'cms') {
+      localStorage.setItem('cmsActive-tab', tabname);
+    } else {
+      localStorage.setItem('portfolioActive-tab', tabname);
+    }
+    this.activetab.emit(this.activeTab);
+  }
+  logOut(){
+    localStorage.clear();
+    this.router.navigate([''])
+  }
 }
